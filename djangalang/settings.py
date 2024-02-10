e9c2242d6547738534c29def3c3b7e02179aae4e
David Morgan <david.r.morgan.92@gmail.com>
2024-02-10 14:24:39 +0000

    remove secret key

--- djangalang/settings.py
+++ djangalang/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-n9%a=@i9+8co+9c2q(qx@58g-404=64!nxflu!p*slk^z0t=%@')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
